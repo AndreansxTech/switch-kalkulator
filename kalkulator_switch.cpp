b84e2643ec5a96cc3315309ecf5b794d26581a71
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-05 21:46:18 +0200

    Update kalkulator_switch.cpp
    Ulepszam kolory żeby lepiej oddawały to co robi program

--- kalkulator_switch.cpp
+++ kalkulator_switch.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <cmath>
+#include <stdlib.h>
 #include <windows.h>
-/*Michal Bankowski
-wiekszosc kodu jest w petli While(true), poniewaz nie trzeba wtedy odpalac programu od nowa za kazdym razem
-wyniki np pierwiastkow podaje z dokladnoscia do 5 miejsc po przecinku */
 using namespace std;
-HANDLE color=GetStdHandle(STD_OUTPUT_HANDLE);
 long double x, y;
-int kolor;
+int kolor=7;
+HANDLE color=GetStdHandle(STD_OUTPUT_HANDLE);
 void kolorki()
 {
-        //int kolor;
+    //int kolor;
     /*if (kolor==11)
         SetConsoleTextAttribute(color,11);
     else if(kolor==12)
@@ -32,6 +30,9 @@ void kolorki()
         case 13:
             SetConsoleTextAttribute(color,13);
             break;
+        case 14:
+            SetConsoleTextAttribute(color,14);
+            break;
         case 6:
             SetConsoleTextAttribute(color,6);
             break;
@@ -41,10 +42,9 @@ void kolorki()
 
     }
 
-    cout<<"kontrolnie kolorek: "<<kolor<<endl;
+    //cout<<"kontrolnie kolorek: "<<kolor<<endl;
     kolor=0;
 }
-
 void menu()
 {
     kolor=11;
@@ -55,15 +55,28 @@ void menu()
 }
 void liczby()
 {
-    cout<<"Podaj pierwsza liczbe: \n";
+    kolor=6; kolorki(); cout<<"\n Podaj pierwsza liczbe: ";
         cin>>x;
 
-    cout<<"Podaj druga liczbe: \n";
+    cout<<"\n Podaj druga liczbe: ";
         cin>>y;
     if(y==0)
-        cout<<"Druga liczba jest zerem co oznacza ze nie bedzie mozna przez nia dzielic!\n";
+    {
+        kolor=12; kolorki();
+        cout<<"Liczba druga jest zerem co oznacza ze nie bedzie mozna przez nia dzielic!\n";
+    }
     else
+    {
         cout<<"Obie liczby sa okej\n";
+        kolorki();
+    }
+
+}
+void informacja()
+{
+    kolor=14; kolorki(); cout<<"[";
+    kolor=12; kolorki(); cout<<"!";
+    kolor=14; kolorki(); cout<<"]";
 }
 
 int main()
@@ -74,6 +87,7 @@ int main()
     while(true)
     {
         char wybor;
+        kolor=7; kolorki();
         cout<<"Wybierz ktoras z opcji z listy: ";
         cin>>wybor;
         cout<<"\n";
@@ -88,31 +102,44 @@ int main()
             liczby();
             break;
         case '1':
-            cout<<"Wybrales dodawanie\n Wynikiem dodawania liczb "<<x<<" oraz "<<y<<" jest liczba "<<x+y<<endl;
+            informacja();
+            cout<<" Wybrales dodawanie\n";
+            kolor=11; kolorki(); cout<<" Wynikiem dodawania liczb "<<x<<" oraz "<<y<<" jest liczba "<<x+y<<endl;
             break;
         case '2':
-            cout<<"Wybrales odejmowanie\n Wynikiem odejmowania liczb "<<x<<" oraz "<<y<<" jest liczba "<<x-y<<endl;
+            informacja();
+            cout<<" Wybrales odejmowanie\n";
+            kolor=11; kolorki();
+            cout<<" Wynikiem odejmowania liczb "<<x<<" oraz "<<y<<" jest liczba "<<x-y<<endl;
             break;
         case '3':
-            cout<<"Wybrales mnozenie\n Wynikiem mnozenia liczb "<<x<<" oraz "<<y<<" jest liczba "<<x*y<<endl;
+            informacja();
+            cout<<" Wybrales mnozenie\n";
+            kolor=11; kolorki();
+            cout<<" Wynikiem mnozenia liczb "<<x<<" oraz "<<y<<" jest liczba "<<x*y<<endl;
             break;
         case '4':
             if(y!=0)
             {
-            cout<<"Wybrales dzielenie calkowite\n Wynikiem dzielenia calkowitego liczb "<<x<<" oraz "<<y<<" jest liczba "<<x/y<<endl;
+            informacja();
+            cout<<" Wybrales dzielenie calkowite\n"; kolor=11; kolorki();
+            cout<<" Wynikiem dzielenia calkowitego liczb "<<x<<" oraz "<<y<<" jest liczba "<<x/y<<endl;
             }
             else
             {
+                kolor=12; kolorki();
                 cout<<"Nie mozna dzielic przez zero !\n Wpisz liczby jeszcze raz\n";
                 liczby();
             }
             break;
         case '5':
-            cout<<"Wybrales pierwiastkowanie\n";
+            informacja();
+            cout<<" Wybrales pierwiastkowanie\n";
             double pierwiastek;
             if (x>=0 && y>=0)
             {
-                cout<<"Wpisz 1 aby pierwiastkowac pierwsza liczbe a 2 aby druga: ";
+                kolor=13; kolorki();
+                cout<<"Z ktorej liczby chcesz obliczyc pierwiastek (wpisz 1 aby pierwiastkowac pierwsza liczbe a 2 aby druga): ";
                 int wybor2;
                 cin>>wybor2;
                 if (wybor2==1)
@@ -132,23 +159,29 @@ int main()
             {
                 cout<<"Poniewaz druga liczba jest ujemna, mozesz zpierwiastkowac tylko pierwsza liczbe poniewaz zadna liczba do kwadratu nie da liczby ujemnej\n";
                 pierwiastek= sqrt (x);
+                kolor=11; kolorki();
                 cout<<"Pierwiastek z pierwszej liczby to "<<pierwiastek<<endl;
             }
             else if(x<0 && y>=0)
             {
-                cout<<"Poniewaz pierwsza liczba jest ujemna, mozesz zpierwiastkowac tylko druga liczbe poniewaz zadna liczba do kwadratu nie da liczby ujemnej\n";
+                cout<<"Poniewaz pierwsza liczba jest ujemna, mozesz zpierwiastkowac druga liczbe poniewaz zadna liczba do kwadratu nie da liczby ujemnej\n";
                 pierwiastek= sqrt (y);
+                kolor=11; kolorki();
                 cout<<"Pierwiastek z drugiej liczby to "<<pierwiastek<<endl;
             }
             else
             {
+                kolor=12; kolorki();
                 cout<<"Zadna liczba do kwadratu nie da liczby ujemnej !\n Nie mozna wykonac tej operacji\n";
             }
             break;
         case '6':
-            cout<<"Wybrales podnoszenie do kwadratu\n Ktora liczbe chcesz podniesc do kwadratu (Wpisz 1 dla pierwszej a 2 dla drugiej): ";
+            informacja();
+            cout<<" Wybrales podnoszenie do kwadratu\n"; kolor=13; kolorki();
+            cout<<" Ktora liczbe chcesz podniesc do kwadratu (Wpisz 1 dla pierwszej a 2 dla drugiej): ";
             int wybor3;
             cin>>wybor3;
+            kolor=11; kolorki();
             if (wybor3==1)
             {
                 cout<<"\n Liczba "<<x<<" podniesiona do kwadratu to "<<x*x<<endl;
@@ -158,34 +191,46 @@ int main()
                 cout<<"\n Liczba "<<y<<" podniesiona do kwadratu to "<<y*y<<endl;
             }
             else
+            {
+                kolor=12; kolorki();
                 cout<<"\n Mozesz wybrac tylko liczbe 1 lub 2\n \n";
+            }
             break;
         case '7':
             if (y!=0)
             {
-            int dziel;
-            dziel = x/y;
-            cout<<" Wybrales dzielenie z reszta\n W dzieleniu z reszta liczby "<<x<<" przez liczbe "<<y<<" otzrymujemy "<<dziel<<" calosci oraz "<<fmod(x,y)<<" reszty "<<endl;
+                int dziel;
+                dziel = x/y;
+                informacja();
+                cout<<" Wybrales dzielenie z reszta\n"; kolor=11; kolorki();
+                cout<<" W dzieleniu z reszta liczby "<<x<<" przez liczbe "<<y<<" otzrymujemy "<<dziel<<" calosci oraz "<<fmod(x,y)<<" reszty "<<endl;
             }
             else
+            {
+                kolor=12; kolorki();
                 cout<<"Nie mozna dzielic przez 0 !\n";
+            }
             break;
         case '8':
             float procenty;
             if(y!=0)
             {
+                informacja();
                 procenty=(x/y)*100;
-                cout<<"Wybrales procenty\n Liczba "<<x<<" stanowi "<<procenty<<"% liczby "<<y<<endl;
+                cout<<" Wybrales procenty\n";
+                kolor=11; kolorki();
+                cout<<" Liczba "<<x<<" stanowi "<<procenty<<"% liczby "<<y<<endl;
             }
             else
             {
-                cout<<"Liczba druga jest zerem wiec nie mozna obliczyc ile procent z niej stanowi liczba "<<x<<endl;
+                cout<<" Liczba druga jest zerem wiec nie mozna obliczyc ile procent z niej stanowi liczba "<<x<<endl;
             }
             break;
         case '9':
             exit(0);
-            break;
+            //break;
         default:
+            kolor=12; kolorki();
             cout<<"Nie ma takiej opcji\n";
             break;
         }
